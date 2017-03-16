5c572bf65a4749e5b70cb2d97efe3a6444a7e4b2
Sukhvinder Singh <sukhvindersingh02@gmail.com>
2017-03-16 17:29:59 -0400

    updat to indext.js

--- index.js
+++ index.js
@@ -11,21 +11,21 @@ restService.use(bodyParser.urlencoded({
 
 restService.use(bodyParser.json());
 
-restService.post('/echo', function(req, res) {
-    var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
-    return res.json({
-        speech: speech,
-        displayText: speech,
-        source: 'webhook-echo-sample'
-    });
-});
+//restService.post('/echo', function(req, res) {
+//    var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
+//    return res.json({
+//        speech: speech,
+//        displayText: speech,
+//        source: 'webhook-echo-sample'
+//    });
+//});
 
 restService.post('/signOn', function(req, res) {
    var speech = "You have successfuly signed in!"
     return res.json({
         speech: speech,
         displayText: speech,
-        source: 'webhook-echo-sample'
+        source: 'cibc'
     });
 });
 
