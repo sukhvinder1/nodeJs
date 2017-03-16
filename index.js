b5de89b7c7eb99c014971bb9bf033cf230e78264
Sukhvinder Singh <sukhvindersingh02@gmail.com>
2017-03-16 17:49:58 -0400

    updat to indext.js

--- index.js
+++ index.js
@@ -21,31 +21,31 @@ restService.use(bodyParser.json());
 //});
 
 restService.post('/signOn', function(req, res) {
-   var speech = ""
+   var speech = "Just like that !"
    
-   var request = require('request');
-
-    var options = {
-      uri: 'https://pilot.api.ebanking.cibc.com/ebm-anp/api/v1/json/sessions',
-      method: 'POST',
-      json: {
-        "card" : {
-            "value" : "4506448426446045",
-            "encrypted" : "false",
-            "encrypt" : "true"
-        },
-          "password" : "potato"
-      }
-    };
-
-    request(options, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-          speech = "You have successfuly signed in!"
-        //console.log(body.id) // Print the shortened url.
-      } else {
-          speech = "na nai chaleya !! "
-      }
-    });
+//   var request = require('request');
+//
+//    var options = {
+//      uri: 'https://pilot.api.ebanking.cibc.com/ebm-anp/api/v1/json/sessions',
+//      method: 'POST',
+//      json: {
+//        "card" : {
+//            "value" : "4506448426446045",
+//            "encrypted" : "false",
+//            "encrypt" : "true"
+//        },
+//          "password" : "potato"
+//      }
+//    };
+//
+//    request(options, function (error, response, body) {
+//      if (!error && response.statusCode == 200) {
+//          speech = "You have successfuly signed in!"
+//        //console.log(body.id) // Print the shortened url.
+//      } else {
+//          speech = "na nai chaleya !! "
+//      }
+//    });
    
    
     return res.json({
