adf0366cb20476f427724dcaecb54f47cec1c18b
Sukhvinder Singh <sukhvindersingh02@gmail.com>
2017-03-16 18:07:21 -0400

    updat to indext.js

--- index.js
+++ index.js
@@ -23,29 +23,36 @@ restService.use(bodyParser.json());
 restService.post('/signOn', function(req, res) {
    var speech = "Just like that !"
    
-//   var request = require('request');
-//
-//    var options = {
-//      uri: 'https://pilot.api.ebanking.cibc.com/ebm-anp/api/v1/json/sessions',
-//      method: 'POST',
-//      json: {
-//        "card" : {
-//            "value" : "4506448426446045",
-//            "encrypted" : "false",
-//            "encrypt" : "true"
-//        },
-//          "password" : "potato"
-//      }
-//    };
-//
-//    request(options, function (error, response, body) {
-//      if (!error && response.statusCode == 200) {
-//          speech = "You have successfuly signed in!"
-//        //console.log(body.id) // Print the shortened url.
-//      } else {
-//          speech = "na nai chaleya !! "
-//      }
-//    });
+   var request = require('request')
+
+    var options = {
+    url: 'https://pilot.api.ebanking.cibc.com/ebm-anp/api/v1/json/sessions',
+    method: "POST",
+    headers: {
+        "WWW-Authenticate": "CardAndPassword",
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "brand": "cibc",
+        "Cookie": "brand=cibc;client_type=mobile_android?os_version=23&app_version=6.0;COOKIE_ACCESS_CHANNEL=MOBILE_ANDROID;eb_version=1.2;"
+    },
+    body: {
+        "card": {
+            "encrypt": true,
+            "encrypted": false,
+            "value": "4506445090048206"
+        },
+        "password": "banking"
+    }
+    }
+
+    request(options, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+          speech = "You have successfuly signed in!"
+        //console.log(body.id) // Print the shortened url.
+      } else {
+          speech = "na nai chaleya !! "
+      }
+    });
    
    
     return res.json({
