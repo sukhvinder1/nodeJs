d0634435a4771c35c89dd3f6dd880e494159d8b6
Sukhvinder Singh <sukhvindersingh02@gmail.com>
2017-08-02 17:50:39 -0400

    test

--- index.js
+++ index.js
@@ -1,396 +1,50 @@
+'use strict';
 
-var express = require('express');
-var bodyParser = require('body-parser');
-var request = require('request');
+const express = require('express')();
+const router = require('express').Router();
+const bodyParser = require('body-parser');
+const ActionsSdkApp = require('actions-on-google').ActionsSdkApp;
+const ApiAiApp = require('actions-on-google').ApiAiApp;
 
-var app = express();
+express.use(bodyParser.json({type: 'application/json'}));
 
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+// In aip.ai console, under Fulfillment set webhook url to
+// https://[YOUR DOMAIN]/example/location
+// don't forget to select "Enable webhook for all domains" for the DOMAIN field
+router.post('/location', (req, res) => {
+	const app = new ApiAiApp({request: req, response: res});
+	const intent = app.getIntent();
 
-app.use(bodyParser.json());
+	switch(intent){
+		case 'input.welcome':
+			// you are able to request for multiple permissions at once
+			const permissions = [
+				app.SupportedPermissions.NAME,
+				app.SupportedPermissions.DEVICE_PRECISE_LOCATION
+			];
+			app.askForPermissions('Your own reason', permissions);
+		break;
+		case 'DefaultWelcomeIntent.DefaultWelcomeIntent-fallback':
+			if (app.isPermissionGranted()) {
+				// permissions granted.
+				let displayName = app.getUserName().displayName;
 
-// User session token
-var xAuthToken = ""
+				//NOTE: app.getDeviceLocation().address always return undefined for me. not sure if it is a bug.
+				// 			app.getDeviceLocation().coordinates seems to return a correct values
+				//			so i have to use node-geocoder to get the address out of the coordinates
+				let coordinates = app.getDeviceLocation().address;
 
-// Server routes
-app.post('/homeHook', function(req, res) {
-    // result.metadata.intentName
-    var intent = req.body.result && req.body.result.metadata && req.body.result.metadata.intentName ? req.body.result.metadata.intentName : ''
-    
-    console.log("Incoming request to /homeHook")
-    console.log("Intent is: " + intent)
-    console.log("\n\n\nRequested Url:")
-    console.log(req.url)
-    console.log("\n\n\nHeaders in the request:")
-    console.log(req.headers)
-    console.log("\n\n\nBody of the request:")
-    console.log(req.body)
-
-    var signOnHandler = function() {
-
-        // Get the card number and password from the request
-        var cardNickname = req.body.result && req.body.result.parameters && req.body.result.parameters.cardNickname ? req.body.result.parameters.cardNickname : ''
-        var password = req.body.result && req.body.result.parameters && req.body.result.parameters.password ? req.body.result.parameters.password : ''
-        // var cardNumber = "4506445090048206"
-        // var password = "banking"
-
-        var cardNumbers = [
-            "4506448426445187",
-            "4506448426445112",
-            "4506448426445252",
-            "4506445644608992"
-        ]
-
-        var cardNumber = ""
-        if (cardNickname.toUpperCase() === "Greg".toUpperCase()) {
-            cardNumber = cardNumbers[0]
-        } else if (cardNickname.toUpperCase() === "Mohit".toUpperCase()) {
-            cardNumber = cardNumbers[1]
-        } else if (cardNickname.toUpperCase() === "Nick".toUpperCase()) {
-            cardNumber = cardNumbers[2]
-        } else if (cardNickname.toUpperCase() === "Colin".toUpperCase() || cardNickname.toUpperCase() === "young money".toUpperCase()) {
-            cardNumber = cardNumbers[3]
-        }
-
-        // Set the callback for the call
-        var callback = function(error, response, body) {
-            if (!error && response.statusCode == 200) {
-                console.log("success")
-                // console.log(body)
-                xAuthToken = response.headers["x-auth-token"]
-                console.log(response.headers["x-auth-token"])
-
-                res.status(200)
-                res.json({
-                    speech: "You have successfully signed on, what would you like to do today ?",
-                    displayText: "You have successfully signed on, what would you like to do today ?",
-                    source: 'CIBC'
-                })
-
-            } else {
-                console.log("failed")
-                console.log(error)
-
-                res.status(200)
-                return res.json({
-                    speech: "Signing on as " + cardNickname + " failed.  Please try again.",
-                    displayText: "Signing on as " + cardNickname + " failed.  Please try again.",
-                    source: 'CIBC'
-                })
-            }
-        }
-
-        // Send the sign on request
-        console.log("Trying to sign in with card number: " + cardNumber)
-        console.log("and password: " + password)
-        signOnRequest(cardNumber, password, callback)        
-
-    }
-
-    var getBalanceHandler = function() {
-
-        // Only continue if we have an x-auth-token already
-        if (xAuthToken != "") {
-
-            var callback = function(error, response, body) {
-                if (!error && response.statusCode == 200) {
-                    console.log("\n\n\n")
-                    console.log("success on accounts")
-                    jsonBody = JSON.parse(body)
-                    // Set balance safely (will not give an exception when null)
-                    var balance = jsonBody.accountGroups[0] && jsonBody.accountGroups[0].accounts[0] ? jsonBody.accountGroups[0].accounts[0].balance : undefined
-                    if (balance != undefined) {
-                        console.log(balance)
-                        var balanceCurrency = balance.currency
-                        var balanceAmount = balance.amount
-                        res.json({
-                            speech: "Your balance is, $" + balanceAmount,
-                            displayText: "Your balance is, $" + balanceAmount,
-                            source: 'CIBC'
-                        })
-                    } else {
-                        // Balance, or the path to it doesn't exist.  Something is wrong with accounts.
-                        res.status(200)
-                        res.json({
-                            speech: "Unable to get balance",
-                            displayText: "Unable to get balance",
-                            source: 'CIBC'
-                        })
-                    }
-                    // console.log(body)
-                } else {
-                    console.log("failed on accounts")
-                    console.log(error)
-
-                    res.status(200)
-                    res.json({
-                        speech: "Unable to get balance",
-                        displayText: "Unable to get balance",
-                        source: 'CIBC'
-                    })
-                }
-            }
-
-            getAccountsRequest(callback)
-
-        } else {
-            res.status(200)
-            res.json({
-                speech: 'You are not signed in, please say "sign in" to sign in first',
-                displayText: 'You are not signed in, please say "sign in" to sign in first',
-                source: 'CIBC'
-            })
-        }
-    }
-
-    var helpIntentHelper = function() {
-
-        var questionContext = req.body.result && req.body.result.parameters && req.body.result.parameters.questioncontext ? req.body.result.parameters.questioncontext : ''
-
-        var response = "Question not recognized, please try again."
-
-        if (questionContext == "customer service") {
-            response = "Please call 1-800-465-2422 for service in English or call 1-888-337-2422 for service in French."
-        } else if (questionContext == "raise salary") {
-            response = "According to my records, you work for CIBC.  Keep up the good work and your wonderful managers will take care of your pay."
-        } else if (questionContext == "edeposit") {
-            response = "First, you must have the CIBC Mobile Banking app and be registered for CIBC Online and Mobile Banking, then sign on."
-        } else if (questionContext == "stolen credit card") {
-            response = "I'm sorry to hear that, we're here to help.  If you believe that your card may have been used fraudulently, contact CIBC Credit Card Services at 1-800-663-4575 in Canada or the U.S., or 514-861-9898 from elsewhere."
-        } else if (questionContext == "fx rate") {
-            response = "To obtain historical or current foreign exchange rates, please contact CIBC Telephone Banking at 1-800-465-2422. Assistance is available 24 hours a day, seven days a week."
-        } 
-
-        res.status(200)
-        res.json({
-            speech: response,
-            displayText: response,
-            source: 'CIBC'
-        })
-    }
-
-    if (intent == 'signIn') {
-        signOnHandler()
-    } else if (intent == 'getBalance') {
-        getBalanceHandler()
-    } 
-    else if (intent == 'help') {
-        helpIntentHelper()
-    } else {
-        res.status(200)
-        res.json({
-            speech: "Command not recognized.  Please try again.",
-            displayText: "Command not recognized.  Please try again.",
-            source: 'CIBC'
-        })
-    }
-})
-
-app.post('/signOn', function(req, res) {
-// app.get('/signOn', function(req, res) {
-
-    console.log("Incoming request to /signOn")
-    console.log("\n\n\nRequested Url:")
-    console.log(req.url)
-    console.log("\n\n\nHeaders in the request:")
-    console.log(req.headers)
-    console.log("\n\n\nBody of the request:")
-    console.log(req.body)
-
-    // Get the card number and password from the request
-    var cardNickname = req.body.result && req.body.result.parameters && req.body.result.parameters.cardNickname ? req.body.result.parameters.cardNickname : ''
-    var password = req.body.result && req.body.result.parameters && req.body.result.parameters.password ? req.body.result.parameters.password : ''
-    // var cardNumber = "4506445090048206"
-    // var password = "banking"
-
-    var cardNumbers = [
-        "4506448426445187",
-        "4506445675079386",
-        "4506448426445252"
-    ]
-
-    var cardNumber = ""
-    if (cardNickname.toUpperCase() === "Greg".toUpperCase()) {
-        cardNumber = cardNumbers[0]
-    } else if (cardNickname.toUpperCase() === "Mohit".toUpperCase()) {
-        cardNumber = cardNumbers[1]
-    } else if (cardNickname.toUpperCase() === "Nick".toUpperCase()) {
-        cardNumber = cardNumbers[2]
-    }
-
-    // Only continue if the required fields are provided
-    if (cardNumber != '' && password != '') {
-        
-
-
-        // Set the callback for the call
-        var callback = function(error, response, body) {
-            if (!error && response.statusCode == 200) {
-                console.log("success")
-                // console.log(body)
-                xAuthToken = response.headers["x-auth-token"]
-                console.log(response.headers["x-auth-token"])
-
-                res.status(200)
-                res.json({
-                    speech: "You have successfully signed on",
-                    displayText: "You have successfully signed on",
-                    source: 'CIBC'
-                })
-
-            } else {
-                console.log("failed")
-                console.log(error)
-
-                res.status(200)
-                return res.json({
-                    speech: "Signing on as " + cardNickname + " failed.  Please try again.",
-                    displayText: "Signing on as " + cardNickname + " failed.  Please try again.",
-                    source: 'CIBC'
-                })
-            }
-        }
-
-        // Send the sign on request
-        signOnRequest(cardNumber, password, callback)        
-
-    } else {
-        res.status(200)
-        res.json({
-            speech: "Card number or password not provided.  Please try again.",
-            displayText: "Card number or password not provided.  Please try again.",
-            source: 'CIBC'
-        })
-    }
-
+				app.tell('Hi ' + app.getUserName().givenName + '! Your address is ' + address);
+			}else{
+				// permissions are not granted. ask them one by one manually
+				app.ask('Alright. Can you tell me you address please?');
+			}
+		break;
+	}
 });
 
-app.post('/getBalance', function(req, res) {
-// app.get('/getBalance', function(req, res) {
+express.use('/example', router);
 
-    console.log("Incoming request to /getBalance")
-    console.log("\n\n\nRequested Url:")
-    console.log(req.url)
-    console.log("\n\n\nHeaders in the request:")
-    console.log(req.headers)
-    console.log("\n\n\nBody of the request:")
-    console.log(req.body)
-
-
-    // Only continue if we have an x-auth-token already
-    if (xAuthToken != "") {
-
-        var callback = function(error, response, body) {
-            if (!error && response.statusCode == 200) {
-                console.log("\n\n\n")
-                console.log("success on accounts")
-                jsonBody = JSON.parse(body)
-                // Set balance safely (will not give an exception when null)
-                var balance = jsonBody.accountGroups[0] && jsonBody.accountGroups[0].accounts[0] ? jsonBody.accountGroups[0].accounts[0].balance : undefined
-                if (balance != undefined) {
-                    console.log(balance)
-                    var balanceCurrency = balance.currency
-                    var balanceAmount = balance.amount
-                    res.json({
-                        speech: "Your balance is, $" + balanceAmount,
-                        displayText: "Your balance is, $" + balanceAmount,
-                        source: 'CIBC'
-                    })
-                } else {
-                    // Balance, or the path to it doesn't exist.  Something is wrong with accounts.
-                    res.status(200)
-                    res.json({
-                        speech: "Unable to get balance",
-                        displayText: "Unable to get balance",
-                        source: 'CIBC'
-                    })
-                }
-                // console.log(body)
-            } else {
-                console.log("failed on accounts")
-                console.log(error)
-
-                res.status(200)
-                res.json({
-                    speech: "Unable to get balance",
-                    displayText: "Unable to get balance",
-                    source: 'CIBC'
-                })
-            }
-        }
-
-        getAccountsRequest(callback)
-
-    } else {
-        res.status(200)
-        res.json({
-            speech: 'You are not signed in, please say "sign in" to sign in first',
-            displayText: 'You are not signed in, please say "sign in" to sign in first',
-            source: 'CIBC'
-        })
-    }
-})
-
-app.get('/testPing', function(req, res) {
-    res.sendStatus(200)
-})
-// End server routes
-
-// api.ai call handlers
-
-// End api.ai call handlers
-
-// eBanking calls
-
-var signOnRequest = function(cardNumber, password, callback) {
-    // Request options
-    var options = {
-        // url: 'https://uat3.www.cibc.mobi/ebm-anp/api/v1/json/sessions',
-        url: 'https://pilot.api.ebanking.cibc.com/ebm-anp/api/v1/json/sessions',
-        method: "POST",
-        headers: {
-            "WWW-Authenticate": "CardAndPassword",
-            "Content-Type": "application/json",
-            "Accept": "application/json",
-            "brand": "cibc",
-            "Cookie": "brand=cibc;client_type=mobile_android?os_version=23&app_version=6.0;COOKIE_ACCESS_CHANNEL=MOBILE_ANDROID;eb_version=1.2;"
-        },
-        body: JSON.stringify({
-            "card": {
-                "encrypt": true,
-                "encrypted": false,
-                "value": cardNumber
-            },
-            "password": "potato"
-        })
-    }
-
-    // Request call
-    request(options, callback)
-}
-
-
-
-var getAccountsRequest = function(callback) {
-    // Request options
-    var options = {
-        // url: 'https://uat3.www.cibc.mobi/ebm-ai/api/v1/json/accountGroups',
-        url: 'https://pilot.api.ebanking.cibc.com/ebm-ai/api/v1/json/accountGroups',
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-            "Accept": "application/json",
-            "brand": "cibc",
-            "X-Auth-Token": xAuthToken,
-            "Cookie": "brand=cibc;client_type=mobile_android?os_version=23&app_version=6.0;COOKIE_ACCESS_CHANNEL=MOBILE_ANDROID;eb_version=1.2;"
-        }
-    }
-
-    request(options, callback)
-}
-
-app.listen((process.env.PORT || 8000), function() {
-    console.log("App up and running, listening.")
+express.listen('8081', function () {
+  console.log('Example app is running')
 })
